3edf5436b9f9bcb44848d0321c8468bcf80c120f
Jayu <jayeshhande00@gmail.com>
2022-05-26 15:17:33 +0530

    edit blog section

---
+++ script.js
@@ -0,0 +1,8 @@
+const button = document.querySelector("#read");
+
+button.addEventListener("click", () => {
+  window.open(
+    "https://jayeshhande.hashnode.dev/javascript-in-the-browser-dom-and-events-fundamentals#heading-event-handler-attributes",
+    "_blank"
+  );
+});

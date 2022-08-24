ca7e92ad5ebd713a456939212d90d9ae0e99a264
Jayu <jayeshhande00@gmail.com>
2022-08-24 09:37:56 +0530

    update blog

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-const button = document.querySelector("#read");
+const buttons = document.querySelectorAll(".readAll");
 
-button.addEventListener("click", () => {
-  window.open(
-    "https://jayeshhande.hashnode.dev/javascript-in-the-browser-dom-and-events-fundamentals#heading-event-handler-attributes",
-    "_blank"
-  );
+buttons.forEach(function (temp) {
+  temp.addEventListener("click", (e) => {
+    if (e.target.id == "read-first") {
+      window.open(
+        "https://jayeshhande.hashnode.dev/javascript-in-the-browser-dom-and-events-fundamentals#heading-event-handler-attributes",
+        "_blank"
+      );
+    }
+    if (e.target.id == "read-second") {
+      window.open("https://jayeshhande.hashnode.dev/server-errors", "_blank");
+    }
+  });
 });
+
+// const button1 = document.querySelector("#read-first");
+
+// button1.addEventListener("click", () => {
+
+// });
